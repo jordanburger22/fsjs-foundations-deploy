ce4137cce442c80b0d5e44313aa9a66cbed7cfb1
Jordan Burger <jordanburger22@gmail.com>
2024-04-04 10:25:25 -0600

    updated styling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const header = document.getElementById('header')
 const themeButton = document.getElementById('theme-btn')
 const blogBtn = document.getElementById('blog-btn')
+const bookmark = document.getElementById('bookmark')
+const moon = document.getElementById('moon')
 
 header.textContent = 'SED ALIQUAM'
 
@@ -8,9 +10,20 @@ themeButton.addEventListener('click', function(){
     if(document.body.style.backgroundColor === 'white'){
         document.body.style.backgroundColor = '#393545'
         document.body.style.color = '#FFFFFF'
+        blogBtn.style.color = '#0F0E12'
+        blogBtn.style.backgroundColor = '#FFFFFF'
+        bookmark.src = '/images/bookmark-black.jpg'
+        themeButton.style.backgroundColor = '#D8D6E1'
+        moon.src = '/images/moon-purple.png'
+
     } else {
         document.body.style.backgroundColor = 'white'
         document.body.style.color = '#0F0E12'
+        blogBtn.style.color = '#FFFFFF'
+        blogBtn.style.backgroundColor = '#0F0E12'
+        bookmark.src = '/images/bookmark-white.png'
+        themeButton.style.backgroundColor = '#FFFFFF'
+        moon.src = '/images/moon-black.jpg'
     }
 })
 
